82cf33609457ab28696925b65ed5c75c18021426
chenshyiuan <chenshyiuan@qq.com>
2017-11-20 22:48:59 +0800

    【更高生命】完善发布活动的发不起，引入bluebird实现Promise

--- pages/activity/setupPosition/setupPosition.js
+++ pages/activity/setupPosition/setupPosition.js
@@ -1,5 +1,9 @@
 // pages/activity/setupPosition/setupPosition.js
-var bmap = require('../../../libs/bmap-wx.js');
+const bmap = require('../../../libs/bmap-wx');
+const { wxPromisify } = require('../../../utils/util');
+const getLocationPromise = wxPromisify(wx.getLocation);
+
+
 Page({
   data: {
     markers: [{
@@ -8,45 +12,83 @@ Page({
       latitude: 23.099994,
       longitude: 113.324520,
       width: 30,
-      height: 30
+      height: 30,
+      callout: {
+        content: '华南师范大学',
+        color: '#808080FF',
+        fontSize: 12,
+        borderRadius: 10,
+        bgColor: '#7FFFD460',
+        padding: 5,
+        display: 'ALWAYS'
+      }
     }],
     circles: [{
       latitude: 23.099994,
       longitude: 113.324520,
       color: '#FFFFE0AA',
       fillColor: '#FFFFE0AA',
-      radius: 200
+      radius: 100
     }],
-    polyline: [],
-    controls: []
+    sugData: [],
+    inputVal: "",
+    position: {},
+    inputShowed: false,
+
   },
 
-  // 绑定input输入 
-  bindKeyInput: function (e) {
-    let that = this;
-    // 新建百度地图对象 
+  onLoad: function (e) {
+
+    //新建百度地图对象
     let BMap = new bmap.BMapWX({
       ak: 'hOfa0G8FQM2LgYxmqSVsu7rUyeS043Np'
     });
-    let fail = function (data) {
-      console.log(data)
-    };
-    let success = function (data) {
-      console.log(data);
-      // var sugData = '';
-      // for (var i = 0; i < data.result.length; i++) {
-      //   sugData = sugData + data.result[i].name + '\n';
-      // }
-      // that.setData({
-      //   sugData: sugData
-      // });
-    }
-    // 发起suggestion检索请求 
-    BMap.suggestion({
-      query: e.detail.value,
-      fail: fail,
-      success: success
+    this.BMap = BMap;
+
+    let that = this;
+    getLocationPromise({
+      type: 'gcj02'
+    }).then(res => {
+      let circle = that.data.circles[0];
+      let marker = that.data.markers[0];
+      circle.latitude = res.latitude;
+      circle.longitude = res.longitude;
+      marker.latitude = res.latitude;
+      marker.longitude = res.longitude;
+
+
+      that.BMap.regeocoding({
+        location: res.latitude.toString() + ',' + res.longitude.toString(),
+        success: function (res) {
+          console.log(res);
+          marker.callout.content = res.wxMarkerData[0].address;
+          that.setData({
+            circles: [circle],
+            markers: [marker],
+          });
+        }
+      });
+
+    }).catch(err => {
+      console.log(err);
     });
+
+
+    // wx.getLocation({
+    //   type: 'gcj02',
+    //   success: function (res) {
+    //     let circle = that.data.circles[0];
+    //     let marker = that.data.markers[0];
+    //     circle.latitude = res.latitude;
+    //     circle.longitude = res.longitude;
+    //     marker.latitude = res.latitude;
+    //     marker.longitude = res.longitude;
+    //     that.setData({
+    //       circles: [circle],
+    //       markers: [marker],
+    //     });
+    //   }
+    // });
   },
 
   sliderChange: function (e) {
@@ -60,10 +102,102 @@ Page({
 
   },
 
-  markerTap: function (e) {
-    console.log(e)
+  showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
   },
-  mapTap: function (e) {
-    console.log(e)
+
+  hideInput: function () {
+    this.setData({
+      inputVal: "",
+      inputShowed: false
+    });
+  },
+
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
+  },
+
+  inputTyping: function (e) {
+    let that = this;
+
+    let fail = function (data) {
+      console.log(data)
+    };
+    let success = function (data) {
+      if (data.result) {
+        that.setData({
+          sugData: data.result
+        });
+      } else {
+        that.setData({
+          sugData: []
+        });
+      }
+    }
+    if (e.detail.value && e.detail.value !== '') {
+      // 发起suggestion检索请求 
+      this.BMap.suggestion({
+        query: e.detail.value,
+        fail: fail,
+        success: success
+      });
+    }
+    this.setData({
+      inputVal: e.detail.value
+    });
+  },
+
+  tapPositionItem: function (e) {
+
+    let index = e.currentTarget.dataset.index;
+    let item = this.data.sugData[index];
+    let circle = this.data.circles[0];
+    let marker = this.data.markers[0];
+    circle.latitude = item.location.lat;
+    circle.longitude = item.location.lng;
+    marker.latitude = item.location.lat;
+    marker.longitude = item.location.lng;
+    marker.callout.content = item.name;
+    if (item) {
+      this.setData({
+        sugData: [],
+        circles: [circle],
+        markers: [marker],
+        position: item,
+        inputVal: "",
+        inputShowed: false
+      });
+    } else {
+      this.setData({
+        sugData: [],
+        inputVal: "",
+        inputShowed: false
+      });
+    }
+    console.log(e);
+  },
+
+  commit: function (e) {
+
+  },
+
+  cancel: function (e) {
+    // wx.chooseLocation({
+    //   success: function (res) {
+    //     console.log(res);
+
+    //   },
+    //   fail: function (res) {
+    //     console.log(res);
+    //   },
+    //   complete: function (res) {
+    //     console.log(res);
+    //   }
+    // });
   }
+
 })
--- utils/util.js
+++ utils/util.js
@@ -1,4 +1,6 @@
 
+const Promise = require('../libs/bluebird');
+
 const formatTime = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
@@ -15,6 +17,25 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+function wxPromisify(fn) {  
+  return function (obj = {}) {    
+    return new Promise((resolve, reject) => {      
+      obj.success = function (res) {        
+        resolve(res)      
+      }      
+
+      obj.fail = function (res) {        
+        reject(res)      
+      }      
+
+      fn(obj)    
+    })  
+  }
+}
+
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,  //以指定格式获取当前时间
+  wxPromisify: wxPromisify,  //将原有的小程序接口替换成
+  Promise: Promise    //以bluebird引入的Promise实现
 }
