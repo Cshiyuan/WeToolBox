ca0fa9ceab79f72c1dfa93199cd378bf057939ea
shiyuan chen <chenshyiuan@qq.com>
2017-12-24 00:48:57 +0800

    【更改说明】添加群二维码的入口。

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,14 +12,14 @@ Page({
 
   onLoad: function () {
 
-    
+
 
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -42,20 +42,29 @@ Page({
     }
   },
 
-  onReady: function() {
-    
+  onReady: function () {
+
     this.toptips = this.selectComponent("#toptips");
     this.noticeBar = this.selectComponent("#noticeBar");
-    this.noticeBar.setContent('只是单纯凭借兴趣制作的工具，任何问题可以加交流群反馈。咩哈哈哈哈哈哈哈哈哈哈哈哈。');
+    this.noticeBar.setContent('只是单纯凭借兴趣制作的工具，任何问题可以加交流群反馈。点下面的连接就有群二维码啦。');
   },
 
-  tapUserInfo: function(e) {
+  lookGroup: function () {
+
+    let url = 'http://share-server.oss-cn-shenzhen.aliyuncs.com/4BE7BDE6792F92F96916AF0FF97F5BF8.jpg';
+    wx.previewImage({
+      current: url, // 当前显示图片的http链接
+      urls: [url] // 需要预览的图片http链接列表
+    });
+  },
+
+  tapUserInfo: function (e) {
     console.log(e);
     this.toptips.showTopTips('');
   },
 
 
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
