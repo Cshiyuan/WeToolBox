5bef3cbaa9a109bd1922b22c578cecc604c4f366
shiyuan chen <chenshyiuan@qq.com>
2018-03-06 16:23:42 +0800

    【更改说明】添加群日记列表页面

--- utils/config.js
+++ utils/config.js
@@ -39,6 +39,9 @@ const UNSTAR_POST_URL = HOST + '/post/unStarPost'; //不点赞帖子
 
 
 const DECRYDATA_URL = HOST + '/group/decryptData';  //解密shareticket
+const GET_USERLIST_BY_GROUP = HOST + '/group/getUserListByGroup';  
+const GET_GROUPLIST_BY_USER = HOST + '/group/getGroupListByUser'; 
+const DELETE_USERGROUP_RELATION = HOST + '/group/deleteUserGroupRelation';
 
 const PICGZ_URL = 'http://wetoolbox-1252042156.picgz.myqcloud.com' //处理图片的URL
 
@@ -84,7 +87,11 @@ module.exports = {
     UNSTAR_POST_URL,
     GET_POSTLIST_ALBUMLIST_URL,
 
+    // 群相关URL
     DECRYDATA_URL,
+    GET_USERLIST_BY_GROUP,
+    GET_GROUPLIST_BY_USER,
+    DELETE_USERGROUP_RELATION,
 
     PICGZ_URL,
     CosConfig
--- utils/groupRequestPromise.js
+++ utils/groupRequestPromise.js
@@ -1,6 +1,9 @@
 const { wxRequestPromise } = require('./util');
 const {
     DECRYDATA_URL,
+    GET_USERLIST_BY_GROUP,
+    GET_GROUPLIST_BY_USER,
+    DELETE_USERGROUP_RELATION
 } = require('./config');
 
 
@@ -19,7 +22,58 @@ function decryptDataPromise(data) {
     });
 }
 
+/**
+ * @description 解密数据
+ * @param 
+ */
+function getUserListPromise(data) {
+
+    return wxRequestPromise({
+        url: GET_USERLIST_BY_GROUP,
+        data: data
+    }).then(response => {
+
+        return response.data;
+    });
+}
+
+/**
+ * @description 解密数据
+ * @param 
+ */
+function getGroupListPromise(data) {
+
+    return wxRequestPromise({
+        url: GET_GROUPLIST_BY_USER,
+        data: data
+    }).then(response => {
+
+        return response.data;
+    });
+}
+
+/**
+ * @description 解密数据
+ * @param 
+ */
+function deleteUserGroupRelationPromise(data) {
+
+    return wxRequestPromise({
+        url: DELETE_USERGROUP_RELATION,
+        data: data
+    }).then(response => {
+
+        return response.data;
+    });
+}
+
+
+
 module.exports = {
 
     decryptDataPromise: decryptDataPromise,  
+    deleteUserGroupRelationPromise:deleteUserGroupRelationPromise,
+    getGroupListPromise: getGroupListPromise,
+    getUserListPromise: getUserListPromise
+
 }
